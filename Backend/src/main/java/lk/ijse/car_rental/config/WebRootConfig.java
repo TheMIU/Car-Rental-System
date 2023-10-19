0ffd5069739c33b73df8b8d43043d1c9018210d5
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2023-10-19 14:35:10 +0530

    spring configure, frontend assets added

---
+++ Backend/src/main/java/lk/ijse/car_rental/config/WebRootConfig.java
@@ -0,0 +1,10 @@
+package lk.ijse.car_rental.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebRootConfig {
+    public WebRootConfig() {
+        System.out.println("WebRootConfig invoked");
+    }
+}
---
+++ Backend/src/main/java/lk/ijse/car_rental/controller/CustomerController.java
@@ -0,0 +1,10 @@
+package lk.ijse.car_rental.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CustomerController {
+    public CustomerController() {
+        System.out.println("CustomerController");
+    }
+}
